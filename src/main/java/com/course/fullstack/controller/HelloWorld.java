a8cf345547fffef190beeea3cdbcadb9613c754a
flaviocarvlh1@gmail.com <flaviocarvlh1@gmail.com>
2023-06-18 10:13:24 +0100

    HelloWord

---
+++ src/main/java/com/course/fullstack/controller/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.course.fullstack.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HelloWorld {
+   
+    @GetMapping
+    String helloWorld() {
+        
+        return "Hello world";
+    
+}
+    
+}
